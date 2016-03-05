8ba3fafc09d08fcf0d57db2b31d6a438ca777f63
Michal Franc <lam.michal.franc@gmail.com>
2016-03-05 23:26:40 +0000

    new d3 graph generation

--- OverSeer.Web/app/components/d3gen.js
+++ OverSeer.Web/app/components/d3gen.js
@@ -1,50 +1,113 @@
 import d3 from 'd3';
 
-export default function generateD3(objects) {
-	var width = window.innerWidth, height = window.innerHeight;
+//http://bl.ocks.org/ericcoopey/6c602d7cb14b25c179a4
 
-	var svg = d3.select("body").append("svg")
-		.attr("width", width)
-		.attr("height", height);
+var width = window.innerWidth, height = window.innerHeight;
 
-	var force = d3.layout.force()
-		.gravity(.05)
-		.distance(10)
-		.charge(-1000)
-		.size([width, height]);
+export function create() {
+	this.addNode = id => {
+		nodes.push({"id": id });
+		update();
+	};
 
-	  force
-		  .nodes(objects.nodes)
-		  .links(objects.links)
-		  .start();
+	this.addLink = (source, target, value) => {
+		links.push({"source": findNode(source), "target": findNode(target), "value": value});
+		update();
+	};
 
-	  var link = svg.selectAll(".link")
-		  .data(objects.links)
-		.enter().append("line")
-		  .attr("class", "link")
-		.style("stroke-width", function(d) { return Math.sqrt(d.weight); });
+	var findNode = function (id) {
+		for (var i in nodes) {
+			if (nodes[i]["id"] === id) return nodes[i];
+		};
+	};
 
-	  var node = svg.selectAll(".node")
-		  .data(objects.nodes)
-		.enter().append("g")
-		  .attr("class", "node")
-		  .call(force.drag);
+	this.addData = data => {
+		data.nodes.forEach(n => {
+			this.addNode(n.name);	
+		});
 
-	  node.append("circle")
-			.attr("class", "status-ok")
-		  .attr("r","7");
+		data.links.forEach(l => {
+			this.addLink(l.target, l.source, 10);
+		});
+	}
 
-	  node.append("text")
-		  .attr("dx", 10)
-		  .attr("dy", ".15em")
-		  .text(function(d) { return d.name });
+	var color = d3.scale.category10();
 
-	  force.on("tick", function() {
-		link.attr("x1", function(d) { return d.source.x; })
-			.attr("y1", function(d) { return d.source.y; })
-			.attr("x2", function(d) { return d.target.x; })
-			.attr("y2", function(d) { return d.target.y; });
+	var graph = d3.select("body")
+			.append("svg:svg")
+			.attr("width", width)
+			.attr("height", height)
+			.attr("id", "svg")
+			.attr("pointer-events", "all")
+			.attr("viewBox", "0 0 " + width + " " + height)
+			.attr("perserveAspectRatio", "xMinYMid")
+			.append('svg:g');
 
-		node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-	  });
+	var force = d3.layout.force();
+	var nodes = force.nodes(), links = force.links();
+
+	var update = function () {
+		var link = graph.selectAll("line")
+				.data(links, d => d.source.id + "-" + d.target.id);
+
+		link.enter().append("line")
+				.attr("id", d => d.source.id + "-" + d.target.id)
+				.attr("stroke-width", d => d.value / 10)
+				.attr("class", "link");
+
+		link.append("title")
+				.text(d => d.value);
+		link.exit().remove();
+
+		var node = graph.selectAll("g.node")
+				.data(nodes, d => d.id);
+
+		var nodeEnter = node.enter().append("g")
+				.attr("class", "node")
+				.call(force.drag);
+
+		nodeEnter.append("svg:circle")
+				.attr("r", 12)
+				.attr("id", d => d.id)
+				.attr("class", "nodeStrokeClass")
+				.attr("fill", d => color(d.id));
+
+		nodeEnter.append("svg:text")
+				.attr("class", "textClass")
+				.attr("x", 14)
+				.attr("y", ".31em")
+				.text(d => d.id);
+
+		node.exit().remove();
+
+		force.on("tick", function () {
+			node.attr("transform", d => {
+				return "translate(" + d.x + "," + d.y + ")";
+			});
+
+			link.attr("x1", d => d.source.x)
+				.attr("y1", d => d.source.y)
+				.attr("x2", d => d.target.x)
+				.attr("y2", d => d.target.y);
+		});
+
+		// Restart the force layout.
+		force
+			.gravity(-.01)
+			.charge(-1000)
+			.friction(0)
+			.linkStrength(0.1)
+			.linkDistance(d => d.value * 15)
+			.size([width, height])
+			.start();
+
+	   	d3.selectAll(".node").forEach(node => {
+            var parent = node[0].parentNode;
+            parent.appendChild(node[0]);
+        });
+	};
+
+	return this;
 }
+
+export default { create }
