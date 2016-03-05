bdc59aaadd642610ee1770931c191581a763e766
Michal Franc <lam.michal.franc@gmail.com>
2016-03-05 23:27:17 +0000

    using fetch to read child endpoints

--- OverSeer.Web/app/components/App.jsx
+++ OverSeer.Web/app/components/App.jsx
@@ -6,12 +6,21 @@ import d3graph from './d3gen.js';
 
 export default class App extends Component {
 	render() {
+		var graph = d3graph.create();
+		var objects = nodeTransformer.objects;
 		fetch('http://localhost:3000/status/health')
-			.then(function(response) {
-				return response.json();
-			}).then(function(json) {
-				var objects = nodeTransformer(json);
-				d3graph(objects);
+			.then(response => response.json())
+			.then(function(json) {
+				json.dependancies.forEach(function (c) {
+					fetch(c.url)
+						.then(resp => resp.json())
+						.then(js => {
+							nodeTransformer.addNodes(js, objects);
+							graph.addData(objects)
+						});
+				});
+				nodeTransformer.addNodes(json, objects);
+			    graph.addData(objects)
 			}).catch(function(ex) {
 				console.log(ex);
 			});
